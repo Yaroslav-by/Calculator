2d8f15013644231489a4d9535b136d2adf6fa044
Yaroslav <asgen-yaroslav@mail.ru>
2022-07-05 21:23:36 +0300

    added reading from file

--- src/Foo.java
+++ src/Foo.java
@@ -1,42 +1,48 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 class Foo {
 
 	public static void main(String[] args) {
     	
-		Scanner sc = new Scanner(System.in);
-		String line = sc.nextLine();
-		String[] a = line.split(" ");
-		
 		try {
-			double first;
-			double second;
 			
-			if (a[0].contains(".")) {
-				first = stringToDouble(a[0]);
-			} else {
-				first = Integer.parseInt(a[0]);
-			}
+			File file = new File("input.txt");
+			Scanner sc = new Scanner(file);
 			
-			if (a[2].contains(".")) {
-				second = stringToDouble(a[2]);
-			} else {
-				second = Integer.parseInt(a[2]);
-			}
-
-			char operation;
-			
-			if (a[1].length() > 1) {
-				throw new OperationException();
-			} else if (a[1].equals("+") || a[1].equals("-") || a[1].equals("*") || a[1].equals("/")) {
-				operation = a[1].charAt(0);
-				if (operation == '/' && second == 0) {
-					throw new ArithmeticException();
+			while (sc.hasNextLine()) {
+				String line = sc.nextLine();
+				String[] a = line.split(" ");
+				
+				double first;
+				double second;
+				
+				if (a[0].contains(".")) {
+					first = Double.parseDouble(a[0]);
+				} else {
+					first = Integer.parseInt(a[0]);
 				}
-			} else {
-				throw new OperationException();
-			}
-			
+				
+				if (a[2].contains(".")) {
+					second = Double.parseDouble(a[2]);
+				} else {
+					second = Integer.parseInt(a[2]);
+				}
+
+				char operation;
+				
+				if (a[1].length() > 1) {
+					throw new OperationException();
+				} else if (a[1].equals("+") || a[1].equals("-") || a[1].equals("*") || a[1].equals("/")) {
+					operation = a[1].charAt(0);
+					if (operation == '/' && second == 0) {
+						throw new ArithmeticException();
+					}
+				} else {
+					throw new OperationException();
+				}
+				
 			switch (operation) {
 				case '+': System.out.println(first + second); 
 			break;
@@ -47,21 +53,19 @@ class Foo {
 				case '/': System.out.println((double) first / second); 
 			break;
 			}
-
+				
+			}
+			sc.close();
 		} catch(NumberFormatException e) {
 			System.out.println("Error! Not number");
 		} catch (OperationException e) {
 			System.out.println("Operation Error!");
 		} catch (ArithmeticException e) {
 			System.out.println("Error! Division by zero");
-		}		
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found!");
+		}
     }
-	
-	public static double stringToDouble(String s) {
-		String[] temp = s.split("\\.");
-		double first = Integer.parseInt(temp[0]);
-		return first = first + (Integer.parseInt(temp[1]) / Math.pow(10, temp[1].length()));
-	}
     
 }
 
